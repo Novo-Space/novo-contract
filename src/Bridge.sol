8695d293257ba54dd380e5a0609caed1f49aa741
JernKunpittaya <61564542+JernKunpittaya@users.noreply.github.com>
2023-01-10 17:56:27 -0800

    bridgeout delay block instead of time

--- src/Bridge.sol
+++ src/Bridge.sol
@@ -6,7 +6,7 @@ import "./wERC20R.sol";
 import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract Bridge {
-    uint256 public delayTime = 1000;
+    uint256 public delayBlock = 12;
     // map erc --> rev erc
     mapping(address => address) revTokenMap;
 
@@ -16,7 +16,7 @@ contract Bridge {
 
     struct withdrawInfo {
         uint256 amt;
-        uint256 startTime;
+        uint256 startBlock;
     }
     //map wallet --> erc20 --> withdrawinfo
     mapping(address => mapping(address => withdrawInfo)) withdrawBalance;
@@ -38,7 +38,7 @@ contract Bridge {
             rToken = new wERC20R(
                 string(abi.encodePacked("rev", underlying.name())),
                 string(abi.encodePacked("r-", underlying.symbol())),
-                3,
+                10,
                 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
             );
             revTokenMap[_erc20Contract] = address(rToken);
@@ -62,15 +62,15 @@ contract Bridge {
         } else {
             withdrawBalance[msg.sender][_erc20Contract].amt += amount;
         }
-        withdrawBalance[msg.sender][_erc20Contract].startTime = block.timestamp;
+        withdrawBalance[msg.sender][_erc20Contract].startBlock = block.number;
     }
 
     function withdrawERC20(address _erc20Contract, uint256 amount) public {
         require(
-            block.timestamp >=
-                withdrawBalance[msg.sender][_erc20Contract].startTime +
-                    delayTime,
-            "Time doesn't pass enough yet"
+            block.number >=
+                withdrawBalance[msg.sender][_erc20Contract].startBlock +
+                    delayBlock,
+            "Time (Block) doesn't pass enough yet"
         );
         require(
             withdrawBalance[msg.sender][_erc20Contract].amt >= amount,
--- test/Bridge.t.sol
+++ test/Bridge.t.sol
@@ -143,7 +143,7 @@ contract WhenAliceBridgeOut is WhenBridgingIn {
             bridgeContract.getwithdrawAmt(from, _erc20Contract),
             transferAmount
         );
-        vm.warp(1100);
+        vm.roll(15);
         bridgeContract.withdrawERC20(_erc20Contract, transferAmount);
         assertEq(bridgeContract.getwithdrawAmt(from, _erc20Contract), 0);
         assertEq(token.balanceOf(alice), mintAmount);
