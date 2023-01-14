8977db0b931c8abb55f2b1c12b463662665059f5
Cole Killian <g.cole.killian@gmail.com>
2023-01-14 12:28:27 -0800

    updates

--- script/Bridge.s.sol
+++ script/Bridge.s.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.0;
 import "forge-std/Script.sol";
 import "../src/Bridge.sol";
+import "../src/Faucet.sol";
 import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract MyScript is Script {
@@ -14,13 +15,21 @@ contract MyScript is Script {
     }
 }
 
+contract DeployFaucet is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        new Faucet(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    }
+}
+
 contract GetNTokenAddresses is Script {
     function run() external {
 
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         // TODO Replace
-        address bridge = 0x75b0B516B47A27b1819D21B26203Abf314d42CCE;
+        address bridge = 0x01cf58e264d7578D4C67022c58A24CbC4C4a304E;
         Bridge bridgeContract = Bridge(bridge);
 
         address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
